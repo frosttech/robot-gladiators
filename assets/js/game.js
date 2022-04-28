6e754177699e064016b5955b03f68defa54e4be0
frosttech <nick@frosttech.dev>
2022-04-28 16:11:34 -0500

    initial game files

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
+// this creates a function named "fight"
+function fight() {
+    console.log("The fight has begun!");
+}
+
+// fight();
